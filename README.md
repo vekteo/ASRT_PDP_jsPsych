dd5dd98e0387bafe2f3b60588c2aa8a536975977
Teodóra Vékony <45353438+vekteo@users.noreply.github.com>
2021-03-09 15:50:47 +0100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # ASRT_PDP_jsPsych
- A Process Dissociation Procedures Task for the ASRT paradigm created with jsPsych.
+A Process Dissociation Procedures Task for the <a href="https://github.com/vekteo/ASRT_JSPsych">ASRT paradigm</a> created with the jsPSych library (<a href="https://link.springer.com/article/10.3758/s13428-014-0458-y">de Leeuw, J. R., 2015</a>).
+
+The task consists of two main parts. In the first part, users have to try to type in the sequence they saw in the ASRT task. In the second part, they have to type in a sequence they did not see during the ASRT task. Now, the appearance of the dogs are controlled by the users. The response keys are the same as in the main task, and the dogs appear according to the button presses.
+
+Each part consist of 4 blocks. Each block is terminated after 24 valid button presses (the 25. button press quits the block).
+
+<h2>Output variables</h2>
+
+- <strong>view_history:</strong> only relavant at the 'instructions'; the actions and the corresponding RTs when the instruction trials are presented
+- <strong>rt:</strong> reaction time (RT) in ms - it indicates the RT from the previous button press
+- <strong>trial_type:</strong> the JSPSych trial type of the given trial ('instructions', 'html-keyboard-response' or 'serial-reaction-time')
+- <strong>trial_index:</strong> the number of the given trials (all events considered, even instructions too!)
+- <strong>time_elapsed:</strong> the time elapsed from the start of the program in ms
+- <strong>internal_code_id:</strong> internal node id of the trial
+- <strong>stimulus:</strong> stimulus on the screen; relevant only if instructions are present
+- <strong>key_press:</strong> the JS code of the key pressed
+- <strong>grid:</strong> layout of the circle positions (in a grid)
+- <strong>target:</strong> position of stimulus on the screen (as a result of the previously pressed button) (the four positions: 0: 0,0; 1: 0,1; 2: 0,2; 3: 0,3)
+- <strong>test_part:</strong> indicates if the trial is part of a valid reponse or if it is the last quitting button press ('valid_button_press', 'last_quitting_trial')
+- <strong>block:</strong> number of the block (1-4)
+- <strong>trial_number:</strong> the number of the element in the response (0-24, only valid responses from 0-23)
+- <strong>response_key:</strong> the pressed response key ('s', 'f', 'j',  'l')
+- <strong>response_button:</strong> the number of the pressed response button (0-3)
+- <strong>task_type:</strong> the type of the task ('inclusion', 'exclusion')
+
+<h2>Setting options</h2>
+<p>The language of the task can be set in the <i>parameters.js</i> file. Available languages: english (en), hungarian (hu)</p>
+
+<h2>Browser requirements</h2>
+<p>Any browser except Safari and Internet Explorer. Recommended: Chrome</p>
---
+++ languages.js
@@ -0,0 +1,70 @@
+const en = {
+    welcomePage: {
+        welcome: "Welcome!",
+        clickNext: "Click on <strong>Next</strong> to continue."
+    },
+    task: {
+        ready: "If you are ready, press <strong>any key</strong> to start the task!",
+    },
+    end:  {
+        end: "End of the task.",
+        thankYou: "Thank you for participating!"
+    },
+    button: {
+        next: "Next",
+        previous: "Previous"
+    },
+    fullscreen: {
+        fullscreenText: "The experiment will switch to full screen mode when you press the button below.",
+        fullscreenButton: "Continue"
+    },
+    pdp: {
+        notRandom: "In the previous task, the order of appearance of the dogs was not completely random. Now we will ask you to try to reproduce the sequence.",
+        control: "In this task, you will be able to control the appearance of the dogs. If you press a response button (S, F, J, or L), the dog will appear at the corresponding position.",
+        reproduce: "Try to reproduce the order in which the dogs appeared in the previous task! Don't worry if you don't now the exact order, it's completely normal. Try to rely on your intuition!",
+        clear: "If the instructions are clear, click on <strong>Next</strong>!",
+        endBlock: "End of the block.",
+        taskSame: "Your task will still be the same.",
+        continue: "Press a key to continue the task!",
+        attention: "ATTENTION!",
+        change: "Now your task will change a little. We will ask you to produce a sequence that <strong>did not appear</strong> in the previous task.",
+        allResponseKeys: "Please use all response keys, and try to avoid repetitions (e.g., SDSDSDSD, SDFJKLSDFJKL)",
+        startTyping: "Start typing the sequence!"
+    }
+}
+
+
+const hu = {
+    welcomePage: {
+        welcome: "Üdvözlünk!",
+        clickNext: "Kattins a <strong>Tovább</strong> gombra a folytatáshoz!"
+    },
+    task: {
+        ready: "Ha készen állsz, nyomd meg <strong>bármelyik</strong> billentyűt a feladat indításához!",
+    },
+    end:  {
+        end: "Vége a feladatnak!",
+        thankYou: "Köszönjük a részvételt!"
+    },
+    button: {
+        next: "Tovább",
+        previous: "Vissza"
+    },
+    fullscreen: {
+        fullscreenText: "A feladat teljes képernyőre ugrik, amint megnyomod a Tovább gombot.",
+        fullscreenButton: "Tovább"
+    },
+    pdp: {
+        notRandom: "Az előző feladatban a kutyák megjelenése nem volt teljesen véletlenszerű. Most a feladatod az lesz, hogy megpróbáld reprodukálni a látott szekvenciát.",
+        control: "Ebben a feladatban most Te fogod irányítani a kutyák megjelenését. Ha lenyomod valamelyik válaszgombot (S, F, J vagy L), a kutya az annak megfelelő pozíción fog megjelenni.",
+        reproduce: "Próbáld meg reprodukálni az a sorrendet, ahogy a kutyák megjelentek az előző feladatban! Ne aggódj, ha nem tudod a pontos sorrendet, ez teljesen normális. Próbálj az intuíciódra hagyatkozni!",
+        clear: "Ha világosak az instrukciók, kattints a <strong>Tovább</strong> gombra!",
+        endBlock: "Vége a blokknak.",
+        taskSame: "A feladatod a továbbiakban is ugyanaz lesz.",
+        continue: "Nyomj meg egy billentyűt a feladat folytatásához!",
+        attention: "FIGYELEM!",
+        change: "Most a feladatod kissé megváltozik. Mostantól arra kérünk, próbálj meg lejátszani egy olyan sorrendet, amelyben <strong>biztos, hogy nem jelentek meg a kutyák </strong> az előző feladatban.",
+        allResponseKeys: "Kérlek, használd az összes válaszgombot, és próbáld meg elkerülni az ismétlődéseket (pl. SDSDSDSD, SDFJKLSDFJKL)!",
+        startTyping: "Kezdd el gépelni a szekvenciát!"
+    }
+}
