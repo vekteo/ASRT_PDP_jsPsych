0235e6c2338f15ddf0f9a5b399b0b79c69e442ea
Teodóra Vékony <45353438+vekteo@users.noreply.github.com>
2021-03-24 13:13:17 +0100

    create language.js
    For the instruction texts in different languages

--- languages.js
+++ languages.js
@@ -5,6 +5,7 @@ const en = {
     },
     task: {
         ready: "If you are ready, press <strong>any key</strong> to start the task!",
+        continue: "If you are ready, press <strong>any key</strong> to continue the task!"
     },
     end:  {
         end: "End of the task.",
@@ -25,7 +26,7 @@ const en = {
         clear: "If the instructions are clear, click on <strong>Next</strong>!",
         endBlock: "End of the block.",
         taskSame: "Your task will still be the same.",
-        continue: "Press a key to continue the task!",
+        continue: "Press a key to continue!",
         attention: "ATTENTION!",
         change: "Now your task will change a little. We will ask you to produce a sequence that <strong>did not appear</strong> in the previous task.",
         allResponseKeys: "Please use all response keys, and try to avoid repetitions (e.g., SDSDSDSD, SDFJKLSDFJKL)",
@@ -41,6 +42,7 @@ const hu = {
     },
     task: {
         ready: "Ha készen állsz, nyomd meg <strong>bármelyik</strong> billentyűt a feladat indításához!",
+        continue: "Ha készen állsz, nyomd meg <strong>bármelyik</strong> billentyűt a feladat folytatásához!"
     },
     end:  {
         end: "Vége a feladatnak!",
@@ -61,10 +63,82 @@ const hu = {
         clear: "Ha világosak az instrukciók, kattints a <strong>Tovább</strong> gombra!",
         endBlock: "Vége a blokknak.",
         taskSame: "A feladatod a továbbiakban is ugyanaz lesz.",
-        continue: "Nyomj meg egy billentyűt a feladat folytatásához!",
+        continue: "Nyomj meg egy billentyűt a folytatáshoz!",
         attention: "FIGYELEM!",
         change: "Most a feladatod kissé megváltozik. Mostantól arra kérünk, próbálj meg lejátszani egy olyan sorrendet, amelyben <strong>biztos, hogy nem jelentek meg a kutyák </strong> az előző feladatban.",
         allResponseKeys: "Kérlek, használd az összes válaszgombot, és próbáld meg elkerülni az ismétlődéseket (pl. SDSDSDSD, SDFJKLSDFJKL)!",
         startTyping: "Kezdd el gépelni a szekvenciát!"
+    }
+}
+
+const fr = {
+    welcomePage: {
+        welcome: "Bienvenue!",
+        clickNext: "Cliquez sur <strong>Suivant</strong> pour continuer."
+    },
+    task: {
+        ready: "Si vous êtes prêt, appuyez sur <strong>n'importe quelle touche</strong> pour commencer la tâche!",
+        continue: "Si vous êtes prêt, appuyez sur <strong>n'importe quelle touche</strong> pour continuer la tâche!"
+    },
+    end:  {
+        end: "Fin de la tâche.",
+        thankYou: "Merci de votre participation!"
+    },
+    button: {
+        next: "Suivant",
+        previous: "Précédent"
+    },
+    fullscreen: {
+        fullscreenText: "L'expérience passera en mode plein écran lorsque vous appuyerez sur le bouton ci-dessous",
+        fullscreenButton: "Continuer"
+    },
+    pdp: {
+        notRandom: "Dans la tâche précédente, l'ordre d'apparition des chiens n'était pas complètement aléatoire. Nous allons maintenant vous demander d'essayer de reproduire la séquence.",
+        control: "Dans cette tâche, vous allez pouvoir contrôler l'apparition des chiens. Si vous appuyez sur un bouton de réponse (S, F, J ou L), le chien apparaîtra à l'endroit correspondant.",
+        reproduce: "Essayez de reproduire l'ordre dans lequel les chiens sont apparus dans la tâche précédente ! Ne vous inquiétez pas si vous ne connaissez pas l'ordre exact, c'est tout à fait normal. Essayez de vous fier à votre intuition !",
+        clear: "Si les instructions sont claires, appuyez sur <strong>Suivant</strong>!",
+        endBlock: "Fin du block.",
+        taskSame: "Votre tâche reste la même.",
+        continue: "Appuyez sur une touche pour continuer!",
+        attention: "ATTENTION!",
+        change: "Maintenant, votre tâche va changer un peu. Nous allons vous demander de produire une séquence qui <strong>n'apparaissait pas</strong> dans la tâche précédente.",
+        allResponseKeys: "Veuillez utiliser toutes les touches pour répondre et essayer d'éviter les répétitions (par exemple, SDSDSDSD, SDFJKLSDFJKL).",
+        startTyping: "Commencez à taper la séquence !"
+    }
+}
+
+const pt = {
+    welcomePage: {
+        welcome: "Bem-vindo!",
+        clickNext: "Clique em <strong>Próximo</strong> para continuar!"
+    },
+    task: {
+        ready: "Se você estiver pronto, aperte <strong>qualquer tecla</strong> para iniciar a tarefa!",
+        continue: "Se você estiver pronto, aperte <strong>qualquer tecla</strong> para continuar a tarefa!"
+    },
+    end:  {
+        end: "Fim da tarefa.",
+        thankYou: "Muito obrigado pela sua participação!"
+    },
+    button: {
+        next: "Próximo",
+        previous: "Anterior"
+    },
+    fullscreen: {
+        fullscreenText: "O experimento irá passar para o modo tela cheia quando você clicar no botão abaixo.",
+        fullscreenButton: "Continue"
+    },
+    pdp: {
+        notRandom: "Na tarefa anterior, a ordem de aparição dos cachorros não era completamente aleatória. Agora nós vamos pedir pra você tentar reproduzir a sequência.",
+        control: "Nesta tarefa, você é quem vai controlar a aparição dos cachorros. Se você apertar uma tecla de resposta (S, F, J, ou L), o cachorro vai aparecer na posição correspondente.",
+        reproduce: "Tente reproduzir a ordem em que os cachorros apareceram na tarefa anterior! Não se preocupe se você não souber a ordem exata, é completamente normal. Tente confiar na sua intuição!",
+        clear: "Se as instruções estão claras, clique em <strong>Próximo</strong>!",
+        endBlock: "Fim do bloco.",
+        taskSame: "Sua tarefa continuará sendo a mesma.",
+        continue: "Aperte uma tecla para continuar!",
+        attention: "ATENÇÃO!",
+        change: "Agora a sua tarefa vai mudar um pouco. Vamos te pedir para produzir uma sequência que <strong>não apareceu</strong> na tarefa anterior.",
+        allResponseKeys: "Utilize por favor todas as teclas de resposta, e tente evitar repetições (ex. SDSDSDSD, SDFJKLSDFJKL).",
+        startTyping: "Comece a digitar a sequência!"
     }
 }
